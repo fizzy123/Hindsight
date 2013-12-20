0f661c1ded282e95c0bea7b17ecff75c7a7492ce
Ambergleam <ambergleam@gmail.com>
2013-12-19 23:25:43 -0500

    Added a bunch of shell classes

--- gen/com/nobelyoo/hindsight/R.java
+++ gen/com/nobelyoo/hindsight/R.java
@@ -8,6 +8,12 @@
 package com.nobelyoo.hindsight;
 
 public final class R {
+    public static final class array {
+        public static final int pref_example_list_titles=0x7f070000;
+        public static final int pref_example_list_values=0x7f070001;
+        public static final int pref_sync_frequency_titles=0x7f070002;
+        public static final int pref_sync_frequency_values=0x7f070003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,25 +23,98 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f0a0002;
+        public static final int action_forgot_password=0x7f0a000d;
+        public static final int action_settings=0x7f0a000e;
+        public static final int email=0x7f0a0008;
+        public static final int item_detail=0x7f0a000c;
+        public static final int item_detail_container=0x7f0a0000;
+        public static final int item_list=0x7f0a0001;
+        public static final int login=0x7f0a000a;
+        public static final int login_form=0x7f0a0007;
+        public static final int login_status=0x7f0a0005;
+        public static final int login_status_message=0x7f0a0006;
+        public static final int password=0x7f0a0009;
+        public static final int progressBarLoad=0x7f0a0004;
+        public static final int sign_in_button=0x7f0a000b;
+        public static final int textView1=0x7f0a0003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_item_detail=0x7f030000;
+        /** 
+    Layout alias to replace the single-pane version of the layout with a
+    two-pane version on Large screens.
+
+    For more on layout aliases, see:
+    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
+    
+
+    Layout alias to replace the single-pane version of the layout with a
+    two-pane version on Large screens.
+
+    For more on layout aliases, see:
+    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
+    
+         */
+        public static final int activity_item_list=0x7f030001;
+        public static final int activity_item_twopane=0x7f030002;
+        public static final int activity_load=0x7f030003;
+        public static final int activity_login=0x7f030004;
+        public static final int fragment_item_detail=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int login=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_forgot_password=0x7f06000b;
+        public static final int action_settings=0x7f060001;
+        public static final int action_sign_in_register=0x7f060009;
+        public static final int action_sign_in_short=0x7f06000a;
+        public static final int app_name=0x7f060000;
+        public static final int error_field_required=0x7f060010;
+        public static final int error_incorrect_password=0x7f06000f;
+        public static final int error_invalid_email=0x7f06000d;
+        public static final int error_invalid_password=0x7f06000e;
+        public static final int hello_world=0x7f060002;
+        public static final int hindsight=0x7f060005;
+        public static final int login_progress_signing_in=0x7f06000c;
+        public static final int pref_default_display_name=0x7f060016;
+        public static final int pref_description_social_recommendations=0x7f060014;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f060018;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f060012;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f06001b;
+        public static final int pref_ringtone_silent=0x7f06001e;
+        public static final int pref_title_add_friends_to_messages=0x7f060017;
+        public static final int pref_title_display_name=0x7f060015;
+        public static final int pref_title_new_message_notifications=0x7f06001c;
+        public static final int pref_title_ringtone=0x7f06001d;
+        public static final int pref_title_social_recommendations=0x7f060013;
+        public static final int pref_title_sync_frequency=0x7f060019;
+        public static final int pref_title_system_sync_settings=0x7f06001a;
+        public static final int pref_title_vibrate=0x7f06001f;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f060007;
+        public static final int prompt_password=0x7f060008;
+        public static final int title_activity_login=0x7f060006;
+        public static final int title_activity_settings=0x7f060011;
+        public static final int title_item_detail=0x7f060004;
+        public static final int title_item_list=0x7f060003;
     }
     public static final class style {
         /** 
@@ -59,10 +138,17 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+        public static final int LoginFormContainer=0x7f080002;
+    }
+    public static final class xml {
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
 }
---
+++ src/com/nobelyoo/hindsight/LoadActivity.java
@@ -0,0 +1,128 @@
+package com.nobelyoo.hindsight;
+
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class LoadActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+
+        // Check for network availability
+        if (isNetworkAvailable() == true) {
+			// Start a new thread that will download all the data
+			new LoadDataTask().execute();
+		} else {
+			// No Internet connection found
+			Toast.makeText(getBaseContext(), "No Internet Connection found", Toast.LENGTH_LONG).show();
+			ProgressBar pb = (ProgressBar) findViewById(R.id.progressBarLoad);
+			pb.setVisibility(ProgressBar.INVISIBLE);
+		}
+        
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                
+            }
+        });
+        thread.start(); 
+	}
+	
+	/*
+	 * Check for Internet connectivity
+	 */
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	 * Goes to the LoginActivity
+	 */
+	private void goLogin() {
+		Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+		startActivity(intent);
+		finish();
+	}
+
+	/*
+	 * Goes to the home screen
+	 */
+	private void goHome() {
+		Intent intent = new Intent(getBaseContext(), ItemListActivity.class);
+		startActivity(intent);
+		finish();
+	}
+	
+	/*
+	 * Loads data in separate thread
+	 */
+	private class LoadDataTask extends AsyncTask<String, Void, Object> {
+		
+		private String result = null;
+		
+		protected Object doInBackground(String... args) {
+			try {
+            	// Create a new HttpClient and Post Header
+            	HttpClient httpClient = new DefaultHttpClient();
+            	HttpGet httpGet = new HttpGet("http://128.61.107.111:56788/users/provide_csrf/");
+            	HttpResponse getResponse = httpClient.execute(httpGet);
+            	
+            	HeaderElement CSRFTOKEN = getResponse.getFirstHeader("Set-Cookie").getElements()[0];
+                HttpPost httpPost = new HttpPost("http://128.61.107.111:56788/users/verify/");
+                httpPost.setHeader("X-CSRFToken", CSRFTOKEN.getValue());
+            	// Execute HTTP Post Request
+                HttpResponse response = httpClient.execute(httpPost);
+                // TODO - change to JSON object
+                result = EntityUtils.toString(response.getEntity());                
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+			return null;
+		}
+
+		/*
+		 * Pass the result data back to the main activity
+		 */
+		protected void onPostExecute(Object result) {
+			sendOnwards();
+		}
+		
+		/*
+		 * Send to the next activity
+		 */
+		private void sendOnwards() {
+			if (result == "True") {
+				// Has connection and credentials
+				goHome();
+            } else {
+            	// Needs to login
+            	goLogin();
+            }
+		}
+
+	}
+	
+}
